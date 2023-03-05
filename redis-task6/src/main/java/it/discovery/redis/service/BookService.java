f908086bd58765a70277fb242a74638337c3c800
Sergiy <sergey.morenets@gmail.com>
2023-03-05 11:02:39 +0200

    Task 10

--- redis-task6/src/main/java/it/discovery/redis/service/BookService.java
+++ redis-task6/src/main/java/it/discovery/redis/service/BookService.java
@@ -1,34 +1,47 @@
 package it.discovery.redis.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import it.discovery.redis.model.Book;
-import it.discovery.redis.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.hash.HashMapper;
+import org.springframework.data.redis.hash.ObjectHashMapper;
 
-@Service
+import java.util.List;
+import java.util.Map;
+
 @RequiredArgsConstructor
 public class BookService {
-	
-	private final BookRepository bookRepository;
 
-	public List<Book> findByName(String name) {
-		return bookRepository.findByName(name); 
-	}
-	
-	public void saveBook(Book book) {
-		bookRepository.save(book);
-	}
-	
-	/**
-	 * Returns all the books where number of pages is greater than pages parameter
-	 * @param name
-	 * @return
-	 */
-	public List<Book> findByPagesGreaterThan(int pages) {
-		//TODO implement
-		return null;
-	}
+    private final RedisTemplate<Integer, ?> redisTemplate;
+
+    private final HashMapper mapper = new ObjectHashMapper();
+
+    public List<Book> findByName(String name) {
+        //TODO
+        return null;
+    }
+
+    public void saveBook(Book book) {
+        HashOperations<Integer, Object, Object> hashOperations = redisTemplate.opsForHash();
+        Map<String, String> map = mapper.toHash(book);
+        hashOperations.putAll(book.getId(), map);
+    }
+
+    public Book findById(int id) {
+        HashOperations<Integer, String, String> hashOperations = redisTemplate.opsForHash();
+        Map<String, String> map = hashOperations.entries(id);
+        return (Book) mapper.fromHash(map);
+    }
+
+    /**
+     * Returns all the books where number of pages is greater than pages parameter
+     *
+     * @param name
+     * @return
+     */
+    public List<Book> findByPagesGreaterThan(int pages) {
+        //TODO implement
+        return null;
+    }
 }
--- redis-task6/src/test/java/it/discovery/redis/service/BookServiceTest.java
+++ redis-task6/src/test/java/it/discovery/redis/service/BookServiceTest.java
@@ -1,26 +1,31 @@
 package it.discovery.redis.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import it.discovery.redis.BaseSpringDataRedisTest;
 import org.junit.jupiter.api.Test;
 
 import it.discovery.redis.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class BookServiceTest {
+public class BookServiceTest extends BaseSpringDataRedisTest {
 
+	@Autowired
 	BookService bookService;
 	
 	@Test
-	void saveBook_findByName_success() {
+	void saveBook_findById_success() {
 		Book book = new Book();
 		book.setNameEn("JPA");
+		book.setId(1);
 		bookService.saveBook(book);
 		
-		List<Book> books = bookService.findByName("JPA");
-		assertEquals(1, books.size());
-		assertEquals("JPA", books.get(0).getNameEn());
+		Book book1 = bookService.findById(book.getId());
+		assertNotNull(book1);
+		assertEquals("JPA", book1.getNameEn());
 	}
 
 }
